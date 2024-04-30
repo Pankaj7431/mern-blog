71934b8ef00f8bb3811221c7caea8cd127f54b5f
Pankaj Kamble <63709568+diablo7431@users.noreply.github.com>
2024-04-30 15:19:41 +0530

    mongodb server connected

--- api/index.js
+++ api/index.js
@@ -1,6 +1,12 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
 
-
+mongoose.connect(process.env.MONGO)
+    .then(() => {
+        console.log('MongoDB is connected');
+    })
 const app = express();
 
 app.listen(3000, () => {
